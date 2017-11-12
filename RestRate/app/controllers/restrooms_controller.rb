faaa0f59c3011853b9975f2d6d66b8050288d8a6
Hira Usman <hirazaheer26@hotmail.com>
2017-11-12 12:55:48 -0500

    Changes after Video 10

--- RestRate/app/controllers/restrooms_controller.rb
+++ RestRate/app/controllers/restrooms_controller.rb
@@ -1,6 +1,25 @@
 class RestroomsController < ApplicationController
 	def index
+	end
 
+	def new
+		@restroom = Restroom.new
+	end
+
+	def create 
+		@restroom = Restroom.new(restroom_params)
+		@restroom.save
+		redirect_to @restroom
+	end
+
+	def show
 
 	end
+	
+	private 
+		def restroom_params
+			params.require(:restroom).permit(:name, :description, :address1, :address2,
+											 :city, :state, :zipcode, :phone, :email)
+		end		
+	
 end
--- RestRate/config/routes.rb
+++ RestRate/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  devise_for :users
+  resources :restrooms
   root 'restrooms#index'
+
+  match '/about_us', to: 'pages#about_us', via: :get
+  match '/contact_us', to: 'pages#contact_us', via: :get
 end
